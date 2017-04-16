0b00b1edbbc2510e2bddf952a16b2fa6189d5f60
VilinPC <vilimanninen@hotmail.com>
2017-04-16 16:19:54 +0300

    Added audio when walking into the gamiina.

---
+++ Assets/CollisionDetect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CollisionDetect : MonoBehaviour
+{
+
+
+    public AudioSource[] auts = new AudioSource[4];
+
+    // Use this for initialization
+    void Start ()
+    {
+        auts[0] = GameObject.Find("Huuto").GetComponent<AudioSource>();
+        auts[1] = GameObject.Find("Huuto2").GetComponent<AudioSource>();
+        auts[2] = GameObject.Find("Huuto3").GetComponent<AudioSource>();
+        auts[3] = GameObject.Find("Huuto4").GetComponent<AudioSource>();
+    }
+    void playOuch()
+    {
+        if (!auts[0].isPlaying && !auts[1].isPlaying && !auts[2].isPlaying && !auts[3].isPlaying)
+        {
+            auts[Random.Range(0, 3)].Play();
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        
+	}
+
+    void OnControllerColliderHit(ControllerColliderHit col)
+    {
+        if (col.gameObject.name == "Pontto" )
+        {
+            playOuch();
+        }
+    }
+}
---
+++ Assets/RandomEvents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class RandomEvents : MonoBehaviour
+{
+    public GameObject target;
+
+
+    void Start()
+    {
+        Invoke("playOuch", 1);
+    }
+
+    void playOuch()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+        float randomTime = Random.Range(1, 5);
+        Invoke("playOuch", randomTime);
+    }
+
+}
